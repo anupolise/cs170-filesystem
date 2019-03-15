3d837538629083285dbb69f7bf87a0ffd3aa7956
Anu Polisetty <apolisetty@umail.ucsb.edu>
2019-03-15 00:01:42 -0700

    working - mount/unmount

--- main.c
+++ main.c
@@ -29,26 +29,13 @@ int make_fs(char *disk_name) {
 
 int mount_fs(char *disk_name) {
 	open_disk(disk_name);
-	// for (int i = 0; i < MAX_FILE_COUNT; i++) {
-	// 	directory[i].fileName = NULL;
-	// 	directory[i].startBlock = -1;
-	// 	directory[i].permission = 0;
-	// }
-	//
-	// for (int i = 0; i < ENTRIES; i++) {
-	// 	FAT[i]= -1;
-	// }
-
 	char* metadata = malloc(BLOCK_SIZE);
 	block_read(0,metadata);
 
 	for(int i = 0; i<MAX_FILE_COUNT; i++){
-		if (i == 0)
-			printf("yo: %d\n", (int)metadata[i*12+4]);
-
-		directory[i].fileName = (char*)metadata[i*12];
-		directory[i].startBlock = *(int*)metadata[i*12+4];
-		directory[i].permission = *(int*)metadata[i*12+8];
+		directory[i].fileName = (char*)metadata+i*12;
+		directory[i].startBlock = *(int*)metadata+i*12+4;
+		directory[i].permission = *(int*)metadata+i*12+8;
 	}
 
 	for(int i=1; i<5; i++){
@@ -57,7 +44,6 @@ int mount_fs(char *disk_name) {
 			FAT[(i-1) * 1024 + j] = (int)metadata[j*4];
 		}
 	}
-	// read metadata from disk
 }
 
 int umount_fs(char *disk_name) {
@@ -66,17 +52,18 @@ int umount_fs(char *disk_name) {
 		for(int i=0; i<MAX_FILE_COUNT; i++){
 			if (directory[i].fileName != NULL)
 				memcpy(bufBytes+i*12, directory[i].fileName,4);
-			if (i == 0)
-				printf("ysdsjkd: %d\n", directory[i].startBlock);
-			sprintf(bufBytes+i*12+4, directory[i].startBlock, 4);
-			sprintf(bufBytes+i*12+8, directory[i].permission, 4);
+
+			memcpy(bufBytes+i*12+4, &directory[i].startBlock, 4);
+			memcpy(bufBytes+i*12+8, &directory[i].permission, 4);
 		}
 
 		int counterBlock = 0;
 		for(int i=0; i<ENTRIES; i++){
-			if((i*4)%4096 == 0)
-				block_write(counterBlock,bufBytes);
-			memcpy(bufBytes+((i*4)%4096), &FAT[i],4);
+			if((i*4)%4096 == 0) {
+				block_write(counterBlock, bufBytes);
+				counterBlock++;
+			}
+			memcpy(bufBytes+((i*4)%4096), &FAT[i], 4);
 		}
 		close_disk();
 }
@@ -85,11 +72,17 @@ int main () {
 	make_fs(DISK_TITLE);
 	mount_fs(DISK_TITLE);
 
+	directory[0].fileName = "123";
 	directory[0].startBlock = 10;
 	directory[0].permission = 9;
+
+	directory[1].fileName = "456";
+	directory[1].startBlock = 756;
+	directory[1].permission = 12324;
 
 	umount_fs(DISK_TITLE);
 	mount_fs(DISK_TITLE);
 
+	while(1);
 	return 0;
 }
